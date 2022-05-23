8748061ddf8f345e2fdaeadb0a9ed5dc2e77e773
Pavel <kulikov-pavlik@bk.ru>
2022-05-23 19:39:53 +0300

    some ads

--- pages/src/api/lotsApi.ts
+++ pages/src/api/lotsApi.ts
@@ -3,12 +3,11 @@ import { HOST } from "../../../config"
 import { Lot, User } from "../types/types"
 import { getSession } from "../utils/authKeyStorageService"
 
-export const addLot = (name:string, price: number) : Promise<AxiosResponse<Lot>> => {
-    debugger;
+export const addLot = (name: string, price: number): Promise<AxiosResponse<Lot>> => {
     const session = getSession();
-    return axios.post(`${HOST}/api/lots/add`, 
-        {"name":name, "startPrice":price, "productsId" : []},
-        {headers: {"x-access-token": session?.token || ""}},
+    return axios.post(`${HOST}/api/lots/add`,
+        { "name": name, "startPrice": price, "productsId": [] },
+        { headers: { "x-access-token": session?.token || "" } },
     )
 }
 
